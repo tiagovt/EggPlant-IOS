b90e0709ff03c4e07429e9851244cca7ed406515
Tiago Vaz <tiago.vt@gmail.com>
2016-05-19 19:54:25 -0300

    commit

---
+++ eggplant-brownie/models/Item.swift
@@ -0,0 +1,20 @@
+//
+//  Item.swift
+//  eggplant-brownie
+//
+//  Created by Tiago Vaz on 19/05/16.
+//  Copyright © 2016 Tiago Vaz. All rights reserved.
+//
+
+import Foundation
+
+
+
+class Item {
+    var name:String
+    var calories:Double
+    init(name: String, calories: Double) {
+        self.name = name
+        self.calories = calories
+    }
+}
