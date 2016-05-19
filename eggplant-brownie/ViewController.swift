854554fe7db0f1ef499d71cb3c8e945e9aae0631
Tiago Vaz <tiago.vt@gmail.com>
2016-05-19 00:03:17 -0300

    Initial Commit

---
+++ eggplant-brownie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by Tiago Vaz on 19/05/16.
+//  Copyright © 2016 Tiago Vaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
